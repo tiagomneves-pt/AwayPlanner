bd115d52683838117721f0bf62ca6ed61d713cb2
Tiago M. Neves <tiagomneves@protonmail.com>
2024-01-19 01:35:29 +0000

    Nova tabela de valores

--- ap.sql
+++ ap.sql
@@ -6,12 +6,12 @@ SET FOREIGN_KEY_CHECKS=0;
 CREATE TABLE `viagens` (
     `id_viagem` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
     `destino` varchar(255) NOT NULL,
-    `partida` varchar(255) NOT NULL DEFAULT 'Coimbra',
+    `partida` varchar(255) NOT NULL DEFAULT DEFAULT,
     `data_viagem` date NOT NULL,
-    `num_passageiros` smallint(5) unsigned NOT NULL DEFAULT 54,
+    `num_passageiros` smallint(5) unsigned NOT NULL DEFAULT DEFAULT,
     `num_inscritos` smallint(5) unsigned NOT NULL DEFAULT 0,
     `custo_total` smallint(5) unsigned NOT NULL,
-    `custo_unit` smallint(5) unsigned NOT NULL,
+    `custo_unit` float unsigned NOT NULL,
     `estado` varchar(10) NOT NULL, --Estado pode ser "Cancelada, Agendada, Confirmada, Realizada"
     `observacoes` varchar(512) DEFAULT NULL,
     PRIMARY KEY (`id_viagem`)
@@ -37,14 +37,42 @@ INSERT INTO `passageiro` (`id`, `nome`, `contacto`) VALUES
 (NULL, 'Silvestre Serra', '910123456'),
 (NULL, 'Álvaro Miranda', '910123456'),
 (NULL, 'Mariano Rocha', '910123456');
+(NULL, 'Catarina Oliveira', '910123456'),
+(NULL, 'Ricardo Almeida', '910123456'),
+(NULL, 'Eduarda Santos', '910123456'),
+(NULL, 'Leonardo Pereira', '910123456'),
+(NULL, 'Beatriz Silva', '910123456'),
+(NULL, 'Henrique Sousa', '910123456'),
+(NULL, 'Isabel Mendes', '910123456'),
+(NULL, 'António Fernandes', '910123456'),
+(NULL, 'Carolina Martins', '910123456'),
+(NULL, 'Gustavo Ribeiro', '910123456');
 
 INSERT INTO `viagens` (`id_viagem`, `destino`, `partida`, `data_viagem`, `num_passageiros`, `custo_total`, `custo_unit`, `estado`, `observacoes`) VALUES 
-(NULL, 'Braga', 'Porto', '2024-01-07', '105', '550', '10', 'Agendada', NULL),
-(NULL, 'Évora', DEFAULT, '2023-12-21', '23', '820', '17', 'Realizada', NULL);
-(NULL, 'Ovar', DEFAULT, '2024-01-14', DEFAULT, '700', '13', 'Confirmada', 'Festival dos pães-de-ló');
+(NULL, 'Faro', DEFAULT, '2023-12-15', 108, 600, 300, 'Realizada', NULL),
+(NULL, 'Braga', DEFAULT, '2024-03-05', DEFAULT, 750, 14, 'Agendada', NULL),
+(NULL, 'Porto', 'Taveiro', '2024-03-10', 108, 750, 100, 'Confirmada', NULL),
+(NULL, 'Lisboa', DEFAULT, '2024-04-01', 216, 800, 100, 'Agendada', NULL),
+(NULL, 'Aveiro', DEFAULT, '2023-12-18', DEFAULT, 400, 15, 'Realizada', NULL),
+(NULL, 'Porto', 'Taveiro', '2024-11-30', 216, 500, 100, 'Agendada', NULL),
+(NULL, 'Évora', DEFAULT, '2024-10-25', DEFAULT, 780, 14.5, 'Agendada', NULL),
+(NULL, 'Évora', DEFAULT, '2023-12-05', 108, 1020, 100, 'Realizada', NULL),
+(NULL, 'Bragança', 'Condeixa', '2024-02-28', DEFAULT, 1100, 15.5, 'Confirmada', NULL),
+(NULL, 'Porto', DEFAULT, '2024-06-10', 108, 700, 100, 'Agendada', NULL),
+(NULL, 'Aveiro', 'Cernache', '2024-04-22', DEFAULT, 540, 16.5, 'Confirmada', NULL),
+(NULL, 'Braga', DEFAULT, '2023-11-18', DEFAULT, 720, 13.5, 'Realizada', NULL),
+(NULL, 'Lisboa', DEFAULT, '2024-07-15', 216, 825, 100, 'Agendada', NULL),
+(NULL, 'Porto', 'Condeixa', '2024-03-05', 216, 717, 100, 'Agendada', NULL),
+(NULL, 'Faro', DEFAULT, '2024-10-10', DEFAULT, 1245, 14, 'Realizada', NULL);
 
-/*
-Silvestre e Mariano foram a Évora
-^Pagou      ^Não pagou
-*/
-INSERT INTO `pagamento` (`id_passageiro`, `id_viagem`, `pago`) VALUES ('1', '2', '1'), ('3', '2', '0');
+INSERT INTO `pagamento` (`id_passageiro`, `id_viagem`, `pago`) VALUES 
+('2', '9', '0'),
+('1', '9', '0'),
+('3', '9', '1'),
+('4', '9', '0'),
+('5', '9', '0'),
+('6', '9', '0'),
+('7', '9', '1'),
+('8', '9', '1'),
+('9', '9', '1'),
+('10', '9', '0');
