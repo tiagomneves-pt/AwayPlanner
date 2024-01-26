0f9081026721a8c92b8e09d74f7f8da663ba7b7f
Tiago M. Neves <tiagomneves@protonmail.com>
2024-01-26 01:25:29 +0000

    Página de contactos reformatada

--- contactos.php
+++ contactos.php
@@ -12,12 +12,11 @@
 <body>
 
 <?php require('includes/menu.html'); ?>
-<!--TODO: Centrar a card-->
 
 <div class="container mt-3 position-absolute top-50 start-50 translate-middle">
-    <div class="card mb-3" style="max-width: 540px;">
+    <div class="card mb-3 position-relative top-50 start-50 translate-middle" style="max-width: 540px;">
         <div class="row g-0">
-            <div class="col-md-4">
+            <div class="col-md-4 p-3">
             <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" fill="#37392E" class="bi bi-telephone-fill mt-4" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/></svg>
             </div>
             <div class="col-md-8">
