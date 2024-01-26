f36b3e7ada40fd61f3b518cab12b5c15d1b0c5ac
Tiago M. Neves <tiagomneves@protonmail.com>
2024-01-26 15:28:24 +0000

    Mapa adicionado à página de contactos

--- contactos.php
+++ contactos.php
@@ -8,27 +8,33 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/cooper-hewitt" type="text/css"/>
     
+
+
 </head>
 <body>
 
 <?php require('includes/menu.html'); ?>
 
 <div class="container mt-3 position-absolute top-50 start-50 translate-middle">
-    <div class="card mb-3 position-relative top-50 start-50 translate-middle" style="max-width: 540px;">
+    <div class="card mt-4 position-relative top-50 start-50 translate-middle" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4 p-3">
-            <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" fill="#37392E" class="bi bi-telephone-fill mt-4" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" fill="#37392E" class="bi bi-telephone-fill mt-4" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/></svg>
             </div>
             <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Contacte-nos</h5>
-                <p class="card-text">Para contacto com nossa equipa técnica, envie um e-mail para o seguinte endereço</p>
-                <p class="text-center">suporte@awayplanner.pt</p>
-                <p class="mb-0">Alternativamente, ligue para a nossa linha 24 horas:</p>
-                <p class="mt-0 text-end">+351 237 000 000</p>
-            </div>
+                <div class="card-body">
+                    <h5 class="card-title">Contacte-nos</h5>
+                    <p class="card-text">Para contacto com nossa equipa técnica, envie um e-mail para o seguinte endereço</p>
+                    <p class="text-center">suporte@awayplanner.pt</p>
+                    <p class="mb-0">Alternativamente, ligue para a nossa linha 24 horas:</p>
+                    <p class="mt-0 text-end">+351 237 000 000</p>
+                </div>
             </div>
         </div>
     </div>
+    <div class="row mt-2">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3046.974832946197!2d-8.455043323448189!3d40.20962276821766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd22f8f0b85baeb7%3A0x190fc4c9387ddaaa!2sCoimbra%20Business%20School%20%7C%20ISCAC!5e0!3m2!1spt-PT!2spt!4v1706281457738!5m2!1spt-PT!2spt" width="600" height="250" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 </div>
+
 </body>
