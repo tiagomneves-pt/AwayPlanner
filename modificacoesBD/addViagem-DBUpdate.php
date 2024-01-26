42953b5fca0c07fea7c619324282af5f84275b78
Tiago M. Neves <tiagomneves@protonmail.com>
2024-01-26 14:08:02 +0000

    Caminhos para os .css corrigidos

--- modificacoesBD/addViagem-DBUpdate.php
+++ modificacoesBD/addViagem-DBUpdate.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AwayPlanner - Adicionar Passageiro</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/cooper-hewitt" type="text/css"/>
     
 </head>
