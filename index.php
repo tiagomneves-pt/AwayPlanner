e25d5ec9d4e1bb5efe90d1fcdd6faaf1f69d45ae
Tiago M. Neves <tiagomneves@protonmail.com>
2024-01-30 02:34:07 +0000

    Formatação da página inicial

--- index.php
+++ index.php
@@ -7,7 +7,49 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/cooper-hewitt" type="text/css"/>
-    
+    <style>
+
+        .opcoes-index{
+            position: relative;
+            z-index: 1;
+        }   
+        .opcoes-index::before {
+            position: absolute;
+            content: '';
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background-image: linear-gradient(
+                45deg,
+                hsla(26, 29%, 74%, 1) 0%,
+                hsla(26, 16%, 62%, 1) 60%,
+                hsla(25, 8%, 51%, 1) 80%,
+                hsla(24, 5%, 39%, 1) 90%,
+                hsla(0, 0%, 27%, 1) 100%);
+            );
+            z-index: -1;
+            transition: opacity 1.5s linear;
+            opacity: 0;
+        }
+
+        .opcoes-index:hover::before{
+            opacity: 1;
+        }
+        #prox-viagem:hover{
+            background: linear-gradient(135deg, hsla(26, 29%, 74%, 1) 0%, hsla(26, 16%, 62%, 1) 60%, hsla(25, 8%, 51%, 1) 80%, hsla(24, 5%, 39%, 1) 90%, hsla(0, 0%, 27%, 1) 100%);
+        }
+        #viagens:hover{
+            background: linear-gradient(225deg, hsla(26, 29%, 74%, 1) 0%, hsla(26, 16%, 62%, 1) 60%, hsla(25, 8%, 51%, 1) 80%, hsla(24, 5%, 39%, 1) 90%, hsla(0, 0%, 27%, 1) 100%);
+        }
+        #analises:hover{
+            background: linear-gradient(225deg, hsla(26, 29%, 74%, 1) 0%, hsla(26, 16%, 62%, 1) 60%, hsla(25, 8%, 51%, 1) 80%, hsla(24, 5%, 39%, 1) 90%, hsla(0, 0%, 27%, 1) 100%);
+        }
+        #contactos:hover{
+            background: linear-gradient(315deg, hsla(26, 29%, 74%, 1) 0%, hsla(26, 16%, 62%, 1) 60%, hsla(25, 8%, 51%, 1) 80%, hsla(24, 5%, 39%, 1) 90%, hsla(0, 0%, 27%, 1) 100%);
+        }
+
+    </style>
 </head>
 <body>
     <?php require('includes/database.php'); ?>
@@ -21,21 +63,21 @@
     ?>
     <div class="row g-0">
         <div class="col-12 col-md-6">
-            <button type="button" class="btn-ap-primary opcoes-index" onclick="redirecionarPagina('passageiros.php?id_v=<?= $prox_viagem->id_viagem?>&destino=<?= $prox_viagem->destino?>')">Próxima viagem: <h1 style="color: #37392E"><?= $prox_viagem->destino?></h1> </button>
+            <button type="button" class="opcoes-index" id="prox-viagem" onclick="redirecionarPagina('passageiros.php?id_v=<?= $prox_viagem->id_viagem?>&destino=<?= $prox_viagem->destino?>')">Próxima viagem: <h1 style="color: #37392E"><?= $prox_viagem->destino?></h1> </button>
         </div>
         <div class="col-12 col-md-6">
-            <button type="button" class="btn-ap-primary opcoes-index" onclick="redirecionarPagina('viagens.php')">Viagens</button>
+            <button type="button" class="opcoes-index" id="viagens" onclick="redirecionarPagina('viagens.php')">Viagens</button>
         </div>
         <div class="col-12 col-md-6">
-            <button type="button" class="btn-ap-primary opcoes-index" onclick="redirecionarPagina('analises.php')">Análises</button>
+            <button type="button" class="opcoes-index" id="analises" onclick="redirecionarPagina('analises.php')">Análises</button>
         </div>
         <div class="col-12 col-md-6">
-            <button type="button" class="btn-ap-primary opcoes-index" onclick="redirecionarPagina('contactos.php')">Apoio técnico</button>
+            <button type="button" class="opcoes-index" id="contactos" onclick="redirecionarPagina('contactos.php')">Apoio técnico</button>
         </div>
     </div>
     
     <div class="position-absolute top-50 start-50 translate-middle">
-        <img src="imagens/rosa-dos-ventos.png" alt="Logótipo da página" class="img-fluid">
+        <img src="imagens/rosa-dos-ventos.png" alt="Logótipo da página" id="logotipo" class="img-fluid" style="position: absolute; z-index:2;">
     </div>
 
 
